ca90e111a875a86f1188d7d01993e5a792504e10
antshater <antshater@gmail.com>
2016-02-11 10:47:23 +0300

    Добавлено 3-ое равно

--- resources/views/grid/column_header_cell.blade.php
+++ resources/views/grid/column_header_cell.blade.php
@@ -1,6 +1,6 @@
 <th ng-show="showColumn('{{ $field }}')" @if(isset($column['class'])) class="{{ $column['class'] }}" @endif>
     <a href="#"
-       @if(!isset($column['sortable']) || $column['sortable'] == true)
+       @if(!isset($column['sortable']) || $column['sortable'] === true)
        ng-click="sort($event, '{{ $field }}')"
        @endif
        ng-class="{
@@ -8,7 +8,7 @@
         }">
 
         {{ $column['title'] }}
-        @if(!isset($column['sortable']) || $column['sortable'] == true)
+        @if(!isset($column['sortable']) || $column['sortable'] === true)
             <i class="fa" ng-class="{
                 'fa-sort-amount-asc': data_provider.sorting.dir == 'asc',
                 'fa-sort-amount-desc': data_provider.sorting.dir == 'desc'
